5f236cc8283b11ad348e3958f5ca07e6601151a5
Romain Lanz <romain.lanz@slynova.ch>
2018-03-09 21:13:40 +0100

    chore: update gitignore and remove mix files

--- webpack.mix.js
+++
@@ -1,7 +0,0 @@
-const mix = require('laravel-mix')
-
-
-mix
-  .copy('resources/assets/img', 'public/img')
-  .js('resources/assets/js/app.js', 'public/js')
-  .sass('resources/assets/sass/app.scss', 'public/css')
