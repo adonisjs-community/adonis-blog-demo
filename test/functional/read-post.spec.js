142cd4949e74ac5b91fa4dbec1b50709ba9cf217
Harminder Virk <virk.officials@gmail.com>
2018-10-04 14:20:05 +0200

    fix(test): use browser for all the tests

--- test/functional/read-post.spec.js
+++ test/functional/read-post.spec.js
@@ -4,33 +4,33 @@ const Factory = use('Factory')
 const { test, trait } = use('Test/Suite')('Read Post')
 
 trait('Auth/Client')
-trait('Session/Client')
+trait('Test/Browser')
 trait('Test/ApiClient')
 trait('DatabaseTransactions')
 
-test("should see an information when there's no post", async ({ assert, client }) => {
+test("should see an information when there's no post", async ({ assert, browser }) => {
   // Given we have no post
 
   // When we fetch the home page
-  const response = await client.get('/').end()
+  const page = await browser.visit('/')
 
   // The request should be good
-  response.assertStatus(200)
+  page.assertStatus(200)
 
   // And we expect to see the message "There's no post available!"
-  assert.include(response.text, "There's no post available!")
+  page.assertHas('There\'s no post available')
 })
 
-test('should see post on home page', async ({ assert, client }) => {
+test('should see post on home page', async ({ assert, browser }) => {
   // Given we already have a post
   const post = await Factory.model('App/Models/Post').create()
 
   // When we fetch the home page
-  const response = await client.get('/').end()
+  const page = await browser.visit('/')
 
   // The request should be good
-  response.assertStatus(200)
+  page.assertStatus(200)
 
   // And we expect to see the title of the created post
-  assert.include(response.text, post.title)
+  page.assertHas(post.title)
 })
