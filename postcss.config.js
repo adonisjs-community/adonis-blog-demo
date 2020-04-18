f3baa43aa4eefc10b874c4c58f654d3a3ce31835
Romain Lanz <romain.lanz+github@pm.me>
2020-04-18 21:44:14 +0200

    style: use TailwindUI styling

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,25 @@
-const path = require('path')
-const tailwindcss = require('tailwindcss')
+const tailwind = require('tailwindcss')
+const autoprefixer = require('autoprefixer')
+const postCSSImport = require('postcss-import')
+const postCSSNested = require('postcss-nested')
+const purgeCSS = require('@fullhuman/postcss-purgecss')
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+const productionPlugins = [
+	purgeCSS({
+		content: ['./resources/views/**/*.edge'],
+		whitelist: ['body', 'html'],
+		whitelistPatterns: [],
+		defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+	}),
+];
 
 module.exports = {
-  plugins: [
-    tailwindcss(path.join(__dirname, './tailwind.config.js'))
-  ]
+	plugins: [
+		postCSSImport(),
+		tailwind(),
+		postCSSNested(),
+		autoprefixer(),
+	].concat(isProduction ? productionPlugins : []),
 }
